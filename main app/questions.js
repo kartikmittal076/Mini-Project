833c0f0b163f64ae6b9cb35c8a37cf95544e40ae
kartikmittal076 <136426066+kartikmittal076@users.noreply.github.com>
2023-12-02 13:28:01 +0530

    add

---
+++ main app/questions.js
@@ -0,0 +1,159 @@
+let questions = [
+
+    {
+    
+    numb: 1,
+    
+    question: "Which is the calm pat of a tropical cyclone?", 
+    answer: "A. Eye",
+    
+    options: [
+    
+    "A. Eye",
+    "B. Limb",
+    "C. Peiphey",
+    "D. None"
+    
+    ]
+    
+    },{
+    numb: 2,
+    
+    question: "Hazards and Disasters are mainly classified as?", 
+    answer: "B. Natural and Human induced",
+    
+    options: [
+    
+    "A. Physical and chemical",
+    "B. Natural and Human induced",
+    "C. Physical and Human",
+    "D. Social and cultural"
+    
+    ]
+    
+    },
+    {
+        numb: 3,
+        
+        question: "A hazard is a situation where there is?",
+        answer: "B. Threat to property and lives from calamities",
+        options: [
+        
+        "A. Threat of natural calamity",
+        "B. Threat to property and lives from calamities",
+        "C. Threat for consequences of disaster",
+        "D. All of the above"
+        
+        ]
+        
+        },
+        {
+            numb: 4,
+            
+            question: "What does HTML stand for?", 
+            answer: "C. Hyper Text Markup Language",
+            options: [
+            
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+            
+            ]
+            
+            },
+            {
+                numb: 5,
+                
+                question: "What does HTML stand for?",
+                     answer: "C. Hyper Text Markup Language",
+                
+                options: [
+                
+                "A. Hyper Type Multi Language",
+                
+                "B. Hyper Text Multiple Language",
+                
+                "C. Hyper Text Markup Language",
+                
+                "D. Home Text Multi Language"
+                
+                ]
+                
+                },
+                {
+                    numb: 6,
+                    
+                    question: "The typical examples of man-made disasters are?", 
+                    answer: "D. All of the above",
+                    options: [
+                    
+                    "A. Chemical explosion",
+                    "B. Leakage of toxic waste",
+                    "C. War and civil strife",
+                    "D. All of the above"
+                    
+                    ]
+                    
+                    },
+                    {
+                        numb: 7,
+                        
+                        question: "Which techniques are used for prediction of early warning of eruption?", 
+                        answer: "D. All of the above",
+                        options: [
+                        
+                        "A. Chemical explosion",
+                        "B. Leakage of toxic waste",
+                        "C. War and civil strife",
+                        "D. All of the above"
+                        
+                        ]
+                        
+                        },
+                        {
+                            numb: 8,
+                            
+                            question: "Shallow earthquakes, less than 20 km deep are associated with", 
+                            answer: "D. All of the above",
+                            options: [
+                            
+                            "A. Chemical explosion",
+                            "B. Divergent plate boundaries",
+                            "C. War and civil",
+                            "D. All of the above"
+                            
+                            ]
+                            
+                            },
+                            {
+                                numb: 9,
+                                
+                                question: "The primary result of earthquakes is?", 
+                                answer: "D. All of the above",
+                                options: [
+                                
+                                "A. Building and bridge collapsed",
+                                "B. Rapture of water and gas pipelines",
+                                "C. Change in course of river and creation of new islands",
+                                "D. All of the above"
+                                
+                                ]
+                                
+                                },
+                                {
+                                    numb: 10,
+                                    
+                                    question: "What are the consequences of disaster on a society?", 
+                                    answer: "D. All of the above",
+                                    options: [
+                                    
+                                    "A. Loss of life",
+                                    "B. Damage to property",
+                                    "C. Environmental Damages",
+                                    "D. All of the above"
+                                    
+                                    ]
+                                    
+                                    },
+]
